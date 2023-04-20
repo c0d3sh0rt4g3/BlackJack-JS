8b1953fa12f16913929e6abbb58e0f5e0ebdb2d0
0ut0f4mm0 <eriking2004@gmail.com>
2023-04-20 13:59:35 +0200

    Half made by console

--- js/index.js
+++ js/index.js
@@ -9,4 +9,49 @@ const cards = ['10_of_clubs.png', '10_of_diamonds.png', '10_of_hearts.png', '10_
     'king_of_diamonds.png', 'king_of_hearts.png', 'king_of_spades.png', 'queen_of_clubs.png', 'queen_of_diamonds.png',
     'queen_of_hearts.png', 'queen_of_spades.png']
 
-alert(cards[5])
+const getRandomCard = () => {
+    const cardIndex = Math.floor(Math.random() * cards.length)
+    const card = cards[cardIndex]
+    cards.splice(cardIndex, 1) // remove card from deck
+    return card
+}
+
+let crupierCards = []
+let crupierPoints
+
+let playerCards = []
+let playerPoints
+
+// Deal initial cards
+crupierCards.push(getRandomCard())
+crupierPoints = calculatePoints(crupierCards)
+
+playerCards.push(getRandomCard())
+playerPoints = calculatePoints(playerCards)
+
+console.log("Crupier's cards:", crupierCards)
+console.log("Crupier's points:", crupierPoints)
+
+console.log("Player's cards:", playerCards)
+console.log("Player's points:", playerPoints)
+
+function calculatePoints(cards) {
+    let points = 0
+    for (const card of cards) {
+        const firstElement = card[0]
+        let cardPoints = 0
+        if (!isNaN(firstElement)) { // number card
+            cardPoints = parseInt(firstElement)
+        } else if (firstElement !== 'a') {
+            cardPoints = 10
+        } else {
+            if(points + 11 <= 21){
+                cardPoints = 11
+            }else{
+                cardPoints = 1
+            }
+        }
+        points += cardPoints
+    }
+    return points
+}
