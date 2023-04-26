d43d3e7155dfd9f3ea38504fe8bc682bb418ff51
Erik López Jiménez <eriking2004@gmail.com>
2023-04-26 21:47:46 +0200

    Refactor of winning check, added better betting

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const cards = ['10_of_clubs.png', '10_of_diamonds.png', '10_of_hearts.png', '10_of_spades.png', '2_of_clubs.png',
+let cards = ['10_of_clubs.png', '10_of_diamonds.png', '10_of_hearts.png', '10_of_spades.png', '2_of_clubs.png',
     '2_of_diamonds.png', '2_of_hearts.png', '2_of_spades.png', '3_of_clubs.png', '3_of_diamonds.png', '3_of_hearts.png',
     '3_of_spades.png', '4_of_clubs.png', '4_of_diamonds.png', '4_of_hearts.png', '4_of_spades.png', '5_of_clubs.png',
     '5_of_diamonds.png', '5_of_hearts.png', '5_of_spades.png', '6_of_clubs.png', '6_of_diamonds.png', '6_of_hearts.png',
@@ -25,98 +25,124 @@ let playerPoints
 let playerBlackJack
 
 let hasBeenPlayed
-let alreadyStopped
 
 let bank = 5000
-let bet = 100
+let bet = 0
 
 let gameWon
-let gameTie
+let gameTie = false
+let gameEnded = false
 
-function playFirstTime(){
-    if (!hasBeenPlayed) {
-        if (bank >= bet) {
-            bank -= bet
-            play()
-        } else {
-            alert("ERROR: You dont have enough money to place this bet")
-        }
-    } else{
+function setBet(betPlaced){
+    if(!hasBeenPlayed){
+        bet += betPlaced
+        document.getElementById("bet").innerHTML = bet
         play()
+        return bet
+    }else{
+        alert("You can only place bets before starting the game")
     }
 }
+
 
 function play(){
-    hasBeenPlayed = true
-    croupierCards.push(getRandomCard())
-    croupierPoints = calculatePoints(croupierCards)
+    if (bet >= 1){
+        if (!gameEnded){
+            let croupierCard = getRandomCard()
+            croupierCards.push(croupierCard)
+            croupierPoints = calculatePoints(croupierCards)
+            const croupierImg = document.createElement("img")
+            croupierImg.src = `./img/${croupierCard}`
+            croupierImg.alt = `${croupierCard}`
+            document.getElementById("croupier").appendChild(croupierImg)
+            hasBeenPlayed = true
+            let playerCard = getRandomCard()
+            playerCards.push(playerCard)
+            playerPoints = calculatePoints(playerCards)
 
-    playerCards.push(getRandomCard())
-    playerPoints = calculatePoints(playerCards)
+            const playerImg = document.createElement("img")
+            playerImg.src = `./img/${playerCard}`
+            playerImg.alt = `${playerCard}`
+            document.getElementById("player").appendChild(playerImg)
 
-    document.getElementById("croupiertxt").textContent = "croupier's cards: " + croupierCards + "croupier's points:" + croupierPoints + "\n"
-    document.getElementById("bank").textContent = bank
-    document.getElementById("playertxt").textContent = "Player's cards: " + playerCards + "\n" + "Player's points:" + playerPoints + "\n"
-    if (playerPoints >= 21 || croupierPoints >= 21) {
-        end()
-        bankCalc()
-        croupierCards = []
-        playerCards = []
-        hasBeenPlayed = ""
-        gameTie = ""
-        gameWon = ""
-    }
-    return hasBeenPlayed
+            document.getElementById("croupierTxt").textContent = "Croupier's points:" + croupierPoints
+            document.getElementById("bank").textContent = bank
+            document.getElementById("playerTxt").textContent = "Player's points:" + playerPoints
+            if (playerPoints >= 21 || croupierPoints >= 21) {
+                // check for player blackjack
+                if (playerPoints === 21 && playerCards.length === 2) {
+                    playerBlackJack = true
+                    end()
+                    bankCalc()
+                    return hasBeenPlayed
+                // check for croupier blackjack
+                }else if(croupierPoints === 21 && croupierCards.length === 2){
+                    croupierBlackJack = true
+                    end()
+                    bankCalc()
+                    return hasBeenPlayed
+                }else {
+                    end()
+                    bankCalc()
+                    return hasBeenPlayed
+                }
+            }
+        }else {alert("You cant keep playing this hand, please click play again before keep playing")}
+    }else {alert("You cant start playing without placing a bet first.")}
 }
 
-function playOnlyCroupier(){
-    if(hasBeenPlayed && !alreadyStopped){
-        alreadyStopped = true
+/*Si el croupier tiene una puntuación de 16 o menos, debe pedir otra carta. Si tiene una puntuación de 17 o más,
+ debe plantarse.*/
+function croupierPlay(){
+    if (croupierPoints <= 16){
         croupierCards.push(getRandomCard())
         croupierPoints = calculatePoints(croupierCards)
+        document.getElementById("croupierTxt").textContent = "Croupier's points: " + croupierPoints
+    }
+}
 
-        document.getElementById("croupiertxt").textContent = "croupier's cards: " + croupierCards + "\n" + "croupier's points: " + croupierPoints + "\n"
+function stand(){
+    if (hasBeenPlayed){
+        croupierPlay()
         end()
         bankCalc()
-        croupierCards = []
-        playerCards = []
-        hasBeenPlayed = ""
-        alreadyStopped = ""
-        gameTie = ""
-        gameWon = ""
-        return alreadyStopped
-    }else {alert("can't")}
+    }else {alert("You can't stand because the game already ended")}
 }
 
 function end(){
     if(playerPoints > 21){
         if(croupierPoints > 21){
             document.getElementById("result").textContent = "There's a tie"
-            return  gameTie = true
+            gameTie = true
         }else{
             document.getElementById("result").textContent = "You lose"
+            gameWon = false
         }
     }else if(playerPoints <= 21 && playerPoints > croupierPoints){
         if (playerBlackJack){
             document.getElementById("result").textContent = "You scored BlackJack, you win."
-        }else if (playerPoints === croupierPoints){
-            if (croupierBlackJack){
-                document.getElementById("result").textContent = "The croupier scored BlackJack, you lose."
-            }else{
-                document.getElementById("result").textContent = "There's a tie."
-                return  gameTie = true
-            }
+            gameWon = true
         }else {
             document.getElementById("result").textContent = "You win."
-            return gameWon = true
+            gameWon = true
         }
     }else if(croupierBlackJack){
         document.getElementById("result").textContent = "The croupier scored BlackJack, you lose."
+        gameWon = false
     }else if(croupierPoints > playerPoints && croupierPoints <= 21){
         document.getElementById("result").textContent = "You lose"
-    }else{
+        gameWon = false
+    }else if (playerPoints === croupierPoints){
+            if (croupierBlackJack){
+                document.getElementById("result").textContent = "The croupier scored BlackJack, you lose."
+                gameWon = false
+            }else{
+                document.getElementById("result").textContent = "There's a tie."
+                gameTie = true
+            }
+        }else{
         document.getElementById("result").textContent = "You win"
-        return gameWon = true
+        gameWon = true
     }
 }
 
@@ -140,14 +166,46 @@ function calculatePoints(cards) {
     }
     if(playerPoints > 21 || croupierPoints > 21 || playerBlackJack || croupierBlackJack){
         end()
+        bankCalc()
     }
     return points
 }
 
 function bankCalc(){
-    if (gameWon){
-        bank += bet * 2
-    }else if (gameTie){
+    if (gameWon && gameTie === false){
         bank += bet
+    }else if (!gameWon && gameTie === false){
+        bank -= bet
     }
+    document.getElementById("bank").textContent = bank
+    gameEnded = true
+}
+
+function reset(){
+    if (gameEnded){
+        document.getElementById("result").textContent = ""
+        document.getElementById("player").innerHTML = ""
+        document.getElementById("playerTxt").innerHTML = ""
+        document.getElementById("croupier").innerHTML = ""
+        document.getElementById("croupierTxt").innerHTML = ""
+        document.getElementById("bet").innerHTML = ""
+        croupierCards = []
+        playerCards = []
+        hasBeenPlayed = ""
+        bet = 0
+        gameWon = ""
+        gameEnded = false
+        gameTie = false
+        cards = ['10_of_clubs.png', '10_of_diamonds.png', '10_of_hearts.png', '10_of_spades.png', '2_of_clubs.png',
+        '2_of_diamonds.png', '2_of_hearts.png', '2_of_spades.png', '3_of_clubs.png', '3_of_diamonds.png', '3_of_hearts.png',
+        '3_of_spades.png', '4_of_clubs.png', '4_of_diamonds.png', '4_of_hearts.png', '4_of_spades.png', '5_of_clubs.png',
+        '5_of_diamonds.png', '5_of_hearts.png', '5_of_spades.png', '6_of_clubs.png', '6_of_diamonds.png', '6_of_hearts.png',
+        '6_of_spades.png', '7_of_clubs.png', '7_of_diamonds.png', '7_of_hearts.png', '7_of_spades.png', '8_of_clubs.png',
+        '8_of_diamonds.png', '8_of_hearts.png', '8_of_spades.png', '9_of_clubs.png', '9_of_diamonds.png', '9_of_hearts.png',
+        '9_of_spades.png', 'ace_of_clubs.png', 'ace_of_diamonds.png', 'ace_of_hearts.png', 'ace_of_spades.png',
+        'jack_of_clubs.png', 'jack_of_diamonds.png', 'jack_of_hearts.png', 'jack_of_spades.png', 'king_of_clubs.png',
+        'king_of_diamonds.png', 'king_of_hearts.png', 'king_of_spades.png', 'queen_of_clubs.png', 'queen_of_diamonds.png',
+        'queen_of_hearts.png', 'queen_of_spades.png']
+
+    }else {alert("You should end the hand before playing again")}
 }
